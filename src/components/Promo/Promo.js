a7662287de13e7fcddbc824a92607b0073e6ba01
Ritis <jimmyelf@gmail.com>
2021-09-05 18:54:48 +0500

    Адаптация стартовой страницы

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -6,7 +6,7 @@ function Promo() {
         <div className="promo">
             <div className="promo__container">
                 <h1 className="promo__title">
-                    Учебный проект студента факультета<br />Веб-разработки.
+                    Учебный проект студента факультета Веб-разработки.
                 </h1>
                 <p className="promo__subtitle">
                     Листайте ниже, чтобы узнать больше про этот проект и его создателя.
