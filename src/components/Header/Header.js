7fa91d7e306bfaf7c8e6f3e303a9a093ee3d5a1f
ritis <perm300ru@yandex.ru>
2021-11-07 14:50:03 +0500

    Обновлена адапитвная верстка, использованы хуки на бургер-меню, обновлена форма поиска, добавлен прелоэдер

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,17 @@ import "./Header.css";
 import {NavLink} from "react-router-dom";
 import logoPath from "../../images/logo.svg";
 import Navigation from "../Navigation/Navigation";
+import {useState} from "react";
 
 
-function Header(isLogin) {
-    isLogin = false;
+function Header() {
+    const [isLogin, setIsLogin] = useState(true);
+
+    const headerClass = `header ${isLogin ? 'header_auth' : ''}`;
 
     let navigation;
 
-    if (isLogin) {
+    if (!isLogin) {
         navigation =
             <ul className="header__nav">
                 <li className="header__li">
@@ -28,7 +31,7 @@ function Header(isLogin) {
     }
 
     return (
-        <header className={isLogin?"header header_auth":"header"}>
+        <header className={headerClass}>
             <div className="header__container">
                 <NavLink className="header__link-logo" to="/">
                     <img className="header__logo" src={logoPath} alt="Логотип"/>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,26 @@
 import {NavLink} from "react-router-dom";
 import "./Navigation.css";
+import {useState} from "react";
 
 
 function Navigation() {
+    const [isBurgerMenu, setIsBurgerMenu] = useState(false);
 
+    const navigationDisplayClass = `navigation__display ${isBurgerMenu ? 'navigation__display_active' : ''}`;
+    const burgerButtonClass = `navigation__burger ${isBurgerMenu ? 'navigation__burger_close' : ''}`;
+    const navigationShadowClass = `navigation__shadow ${isBurgerMenu ? 'navigation__shadow_active' : ''}`;
+
+    const handleClickBurger = () => {
+        if (isBurgerMenu) {
+            setIsBurgerMenu(false);
+        } else {
+            setIsBurgerMenu(true);
+        }
+    }
 
     return (
         <nav className="navigation">
-            <div className="navigation__display">
+            <div className={navigationDisplayClass}>
                 <ul className="navigation__nav">
                     <li className="navigation__li">
                         <NavLink className="navigation__link navigation__link_index" to="/">
@@ -30,8 +43,8 @@ function Navigation() {
                     <div className="navigation__account-image"> </div>
                 </NavLink>
             </div>
-            <button type="button" className="navigation__burger navigation__burger_close"> </button>
-            <div className="navigation__shadow"> </div>
+            <button type="button" className={burgerButtonClass} onClick={handleClickBurger}> </button>
+            <div className={navigationShadowClass}> </div>
         </nav>
     )
 }
